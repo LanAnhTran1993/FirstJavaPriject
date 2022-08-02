b6d42f0476e5e7bcbfbf081bf32fcc01f64766d8
LanAnhTran1993 <84260587+LanAnhTran1993@users.noreply.github.com>
2022-08-02 11:02:53 +0900

    initial commit

---
+++ FirstProject/src/FirstProject.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class FirstProject {//class
+
+	public static void main(String[] args) { 
+		//method. Identifier for this method is main. Void (nothing) is the return tyoe.
+		//public means it is available everywhere
+		//static means we don't have to create an object to evoke this class
+		System.out.println("Hello World");	
+		//Scanner in = new Scanner(System.in);
+		//String s = in.nextLine();
+		char t = 't';
+		System.out.println(t);
+		//ßtring t = new String("hey"); //evoke the constructor
+	}
+//class -> instantiation -> object1
+	//	-> instantiation -> object2
+}
